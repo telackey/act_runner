d1a9675754a6579fd7a5f6c8d9705cee51e3aaea
Thomas E Lackey <telackey@bozemanpass.com>
2023-03-24 17:59:11 -0500

    Retry registration.

--- run.sh
+++ run.sh
@@ -15,12 +15,28 @@ if [[ ! -s .runner ]]; then
     insecure="--insecure"
   fi
 
+  try=$((try + 1))
+  success=0
+
+  while [[ $success -eq 0 ]] && [[ $try -lt ${GITEA_MAX_REG_ATTEMPTS:-10} ]]; do
   act_runner register \
     --instance "${GITEA_INSTANCE_URL}" \
     --token    "${GITEA_RUNNER_REGISTRATION_TOKEN}" \
     --name     "${GITEA_RUNNER_NAME:-`hostname`}" \
     --labels   "${GITEA_RUNNER_LABELS}" \
-    ${insecure} --no-interactive
+    ${insecure} --no-interactive > /tmp/reg.log 2>&1
+
+    cat /tmp/reg.log
+
+    cat /tmp/reg.log | grep 'Runner registered successfully' > /dev/null
+    if [[ $? -eq 0 ]]; then
+      echo "SUCCESS"
+      success=1
+    else
+      echo "Waiting to retry ..."
+      sleep 5
+    fi
+  done
 fi
 
 act_runner daemon
