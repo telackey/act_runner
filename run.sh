8829c52835d593ba4c4062a022a11f27332a14dd
Thomas E Lackey <telackey@bozemanpass.com>
2023-03-24 16:49:36 -0500

    Simplify script.

--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
-#
-# ADAPTED FROM:
-#
-# SPDX-FileCopyrightText: © Vegard IT GmbH (https://vegardit.com)
-# SPDX-FileContributor: Sebastian Thomschke
-# SPDX-License-Identifier: Apache-2.0
-# SPDX-ArtifactOfProjectHomePage: https://github.com/vegardit/docker-gitea-act-runner
-#
 
 if [[ ! -d /data ]]; then
   mkdir -p /data
@@ -14,20 +6,21 @@ fi
 
 cd /data
 
+# Use the same ENV variable names as https://github.com/vegardit/docker-gitea-act-runner
+# See env.example for example values.
+
 if [[ ! -s .runner ]]; then
-  if [[ ${GITEA_INSTANCE_INSECURE:-} == '1' ]]; then
-    insecure_flag=--insecure
+  insecure=""
+  if [[ ${GITEA_INSTANCE_INSECURE} == '1' ]] || [[ ${GITEA_INSTANCE_INSECURE} == 'true' ]]; then
+    insecure="--insecure"
   fi
-  if [[ -z ${GITEA_RUNNER_REGISTRATION_TOKEN:-} ]]; then
-    read -r GITEA_RUNNER_REGISTRATION_TOKEN < "$GITEA_RUNNER_REGISTRATION_TOKEN_FILE"
-  fi
+
   act_runner register \
     --instance "${GITEA_INSTANCE_URL}" \
     --token    "${GITEA_RUNNER_REGISTRATION_TOKEN}" \
-    --name     "${GITEA_RUNNER_NAME}" \
+    --name     "${GITEA_RUNNER_NAME:-`hostname`}" \
     --labels   "${GITEA_RUNNER_LABELS}" \
-    $( [[ ${GITEA_INSTANCE_INSECURE:-} == '1' ]] && echo "--insecure" || true) \
-    --no-interactive
+    ${insecure} --no-interactive
 fi
 
 act_runner daemon
